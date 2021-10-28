4c79e82adca12ec1e3437fe6c13742d89a8e37f4
Marisabel <72731147+immarisabel@users.noreply.github.com>
2021-10-28 15:36:23 +0200

    thymeleaf works

--- src/main/java/nl/marisabel/Letters/controllers/TestController.java
+++ src/main/java/nl/marisabel/Letters/controllers/TestController.java
@@ -1,15 +1,23 @@
 package nl.marisabel.Letters.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TestController {
 
-    @ResponseBody
-    @GetMapping("/hello/{name}")
-    public String hello(@PathVariable("name") String name){
-        return "hello " + name + "!";
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name", "Marisabel");
+        return "index";
     }
+
+//    @ResponseBody
+//    @GetMapping("/hello/{name}")
+//    public String hello(@PathVariable("name") String name){
+//
+//        return "hello " + name + "!";
+//    }
 
 }
